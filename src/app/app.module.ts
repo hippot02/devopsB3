8de5a05d673b89f92a8cb3991a704ed18a07bb7c
Hippot <hippot02@gmail.com>
2024-01-08 12:34:34 +0100

    feur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ImageFunComponent } from './components/image-fun/image-fun.component';
+import { FeurBoutonComponent } from './feur-bouton/feur-bouton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
     ImageFunComponent,
+    FeurBoutonComponent,
   ],
   imports: [
     BrowserModule
